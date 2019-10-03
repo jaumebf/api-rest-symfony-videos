44a64f84af11f4eabf2a3d414ae88d4e9286362f
jaume <jaumewowcore>
2019-10-03 13:09:55 +0200

    Modelo y controlador de usuarios

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,15 +3,57 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
-class UserController extends AbstractController
-{
+
+use App\Entity\User;
+use App\Entity\Video;
+
+class UserController extends AbstractController {
     
-    public function index()
-    {
-        return $this->json([
+    private function resjson($data){
+        //Serializar datos con servicios serializer
+        $json = $this->get('serializer')->serialize($data, 'json');
+        
+        //Response con httpfoundation
+        $response = new Response();
+        
+        //Asignar contenido a la respuesta
+        $response->setContent($json);
+        
+        //Indicar formato de respuesta
+        $response->headers->set('Content-Type', 'application/json');
+        
+        //Devolver la respuesta
+        return $response;
+    }
+
+
+    public function index() {
+
+        $user_repo = $this->getDoctrine()->getRepository(User::class);
+        $video_repo = $this->getDoctrine()->getRepository(Video::class);
+
+        $users = $user_repo->findAll();
+        $user = $user_repo->find(1);
+
+        $videos = $video_repo->findAll();
+        
+        $data = [
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
-        ]);
+        ];
+
+//        foreach ($users as $user){
+//            echo "<h1>{$user->getName()} {$user->getSurname()}</h1>";
+//            
+//            foreach ($user->getVideos() as $video){
+//                echo "<p>{$video->getTitle()} - {$video->getUser()->getEmail()}</p>";
+//            }
+//        }
+        
+        return $this->resjson($data);
     }
+
 }
